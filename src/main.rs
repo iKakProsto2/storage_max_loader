144f5f98b5fea007da41130704375587b3d472e3
adatan <adatan@protonmail.com>
2021-01-28 03:40:28 +0300

    updated to 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,128 @@
 use std::io::Write;
 use std::collections::HashMap;
+use std::fs::{create_dir, remove_dir_all};
+use std::fs::File;
+use std::ffi::CString;
 use winapi::um::fileapi::{GetLogicalDrives, GetDiskFreeSpaceA};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-const KB_4_DATA: [u8; 4096] = [255u8; 4096];
-const KB_1024_DATA: [u8; 1_048_576] = [255u8; 1_048_576];
-static GB: u64 = 1_073_741_824;
-static GB_70: u64 = GB * 70;
-const DRIVE_LETTERS: [&str; 26] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+#[allow(non_upper_case_globals)]
+static GiB: u64 = 1_073_741_824;
+
+enum HowToFill {
+    WriteAll,
+    WriteByChunk
+}
 
 fn main() {
-    println!("By Adatan current version: {}", VERSION);
+    println!("By Adatan. current version: {}", VERSION);
     let mut drive_letters = HashMap::new();
     let mut count = 0u8;
     let mut f = unsafe { GetLogicalDrives() };
-    if f != 0 {
-        println!("Список доступных дисков на выбранном устройстве:");
-        for i in 0usize..32 {
-            if f % 2 == 1 {
-                count += 1;
-                let drive_letter = format!(r"{}:\", DRIVE_LETTERS[i]);
-                println!("{}) {}", count, &drive_letter);
-                drive_letters.insert(count, drive_letter);
-            }
-            f /= 2;
-        }
+    if f == 0 {
+        panic!("Не обнаружено дисков");
     }
-    let drive_letter = drive_letters[&get_input("Выберите номер диска для заполнения: ").parse::<u8>().unwrap()].clone();
+    println!("Список доступных дисков на выбранном устройстве:");
+    for i in 0usize..32 {
+        if f % 2 == 1 {
+            count += 1;
+            let drive_letter = format!(r"{}:\", char::from(65 + i as u8));
+            println!("{}) {}", count, &drive_letter);
+            drive_letters.insert(count, drive_letter);
+        }
+        f /= 2;
+    }
+    let mut drive_letter = get_input("Выберите номер диска или букву диска (пример: С) для заполнения: ");
+    drive_letter = match drive_letter.parse::<u8>() {
+        Ok(drive_number) => drive_letters.get(&drive_number).unwrap().clone(),
+        Err(_) => format!(r"{}:\", drive_letter.to_uppercase())
+    };
     println!("Выбран диск: {}", drive_letter);
     let mut sectors_per_cluster = 0u32;
     let mut bytes_per_sector = 0u32;
     let mut number_of_free_cluster = 0u32;
     let mut total_number_of_cluster = 0u32;
-    if unsafe { GetDiskFreeSpaceA(#[allow(temporary_cstring_as_ptr)] std::ffi::CString::new(drive_letter.clone()).unwrap().as_ptr(), &mut sectors_per_cluster, &mut bytes_per_sector, &mut number_of_free_cluster, &mut total_number_of_cluster) } == 1 {
-        let cluster_size = bytes_per_sector as u64 * sectors_per_cluster  as u64;
-        let total_disk_space = cluster_size  as u64 * total_number_of_cluster  as u64;
-        let free_disk_space = cluster_size  as u64 * number_of_free_cluster  as u64;
-        println!("Размер кластера выбранного диска: {}", cluster_size);
-        println!("Объем диска: {} гб.", total_disk_space / GB);
-        println!("Объем доступной памяти: {} гб.", free_disk_space / GB);
-        println!("Кол-во секторов на кластер: {}", sectors_per_cluster);
-        println!("Кол-во байтов на сектор: {}", bytes_per_sector);
-        println!("Кол-во свободных кластеров: {}", number_of_free_cluster);
-        println!("Кол-во всех кластеров: {}", total_number_of_cluster);
-        let data_for_file = match get_input("1) Заполнять каждый файл до 1 гб по 4 кб\n2) Заполнять каждый файл до 1 гб по 1024 кб").parse::<u8>().unwrap() {
-            1 => &KB_4_DATA[..],
-            2 => &KB_1024_DATA[..],
-            _ => panic!("Только 1 или 2")
-        };
-        let file_path = format!(r"{}garbage\", drive_letter);
-        std::fs::create_dir(file_path.clone()).unwrap();
-        let mut total_write_size = 0u64;
-        println!("Начинаю заполнение до 70 гб свободного остатка...");
-        let start = get_unix_timestamp();
-        while free_disk_space - total_write_size > GB_70 {
-            let mut file_size = 0u64;
-            let mut file_path = file_path.clone();
-            file_path.push_str(get_unix_milli_timestamp().to_string().as_str());
-            let mut file = std::fs::File::create(file_path).unwrap();
-            while let Ok(b) = file.write(data_for_file) {
-                if b == 0 {
-                    break;
-                }
-                file_size += b as u64;
-                if file_size >= GB {
-                    break;
-                }
-            }
-            total_write_size += file_size;
-        }
-        let end = get_unix_timestamp();
-        println!("Заполнение длилось {} сек.", end - start);
-        pause();
+    if unsafe { GetDiskFreeSpaceA(#[allow(temporary_cstring_as_ptr)] CString::new(drive_letter.clone()).unwrap().as_ptr(), &mut sectors_per_cluster, &mut bytes_per_sector, &mut number_of_free_cluster, &mut total_number_of_cluster) } != 1 {
+        panic!("err: winapi func call GetDiskFreeSpaceA is finished not successfully")
     }
+    let cluster_size = bytes_per_sector as u64 * sectors_per_cluster as u64;
+    let total_disk_space = cluster_size as u64 * total_number_of_cluster as u64;
+    let free_disk_space = cluster_size as u64 * number_of_free_cluster as u64;
+    println!("Размер кластера выбранного диска: {} б", cluster_size);
+    println!("Объем диска: {} GiB", total_disk_space / GiB);
+    println!("Объем доступной памяти: {} GiB", free_disk_space / GiB);
+    println!("Кол-во секторов на кластер: {}", sectors_per_cluster);
+    println!("Кол-во байтов на сектор: {} B", bytes_per_sector);
+    println!("Кол-во свободных кластеров: {}", number_of_free_cluster);
+    println!("Кол-во всех кластеров: {}", total_number_of_cluster);
+    let total_garbage_size = get_bytes_size("Введите какой объем диска нужно заполнить(пример: 1024Б, 1024KB, 1024MB, 1024GB, 1024KiB, 1024MiB, 1024GiB): ");
+    let how_to_fill = match get_input("1) Делать записи по чанкам\n2) Записывать сразу весь файл").as_str() {
+        "1" => HowToFill::WriteByChunk,
+        "2" => HowToFill::WriteAll,
+        _ => panic!("err: unknown method of fill")
+    };
+
+    let garbage_file_size = get_bytes_size( match how_to_fill {
+        HowToFill::WriteAll => "Какого размера будет каждый файл?(пример: 1024Б, 1024KB, 1024MB, 1024GB, 1024KiB, 1024MiB, 1024GiB): ",
+        HowToFill::WriteByChunk => "Какого размера будет чанк для заполнения каждого файла?(пример: 1024Б, 1024KB, 1024MB, 1024GB, 1024KiB, 1024MiB, 1024GiB): "
+    });
+    let garbage_data = vec![0u8; garbage_file_size as usize];
+    let file_path = format!(r"{}garbage\", drive_letter);
+    println!("Пытаюсь создать папку для хранения бессмысленных данных...");
+    if let Err(_) = create_dir(file_path.clone()) {
+        println!("Возможно, папка с файлами уже существует, пробуем удалить папку со всеми файлами...");
+        if let Ok(_) = remove_dir_all(file_path.clone()) {
+            println!("Папка удалена, создаем новую...");
+            create_dir(file_path.clone()).unwrap();
+            println!("Папка создана")
+        }
+    };
+    let mut count = 0u64;
+    let work_time = match how_to_fill {
+        HowToFill::WriteByChunk => {
+            let mut total_wrote_size = 0u64;
+            let start = get_unix_timestamp();
+            while total_wrote_size < total_garbage_size {
+                let mut file_size = 0u64;
+                let mut file_path = file_path.clone();
+                file_path.push_str(count.to_string().as_str());
+                let mut file = File::create(file_path).unwrap();
+                while let Ok(b) = file.write(&garbage_data) {
+                    if b == 0 {
+                        break;
+                    }
+                    file_size += b as u64;
+                    if file_size >= garbage_file_size {
+                        break;
+                    }
+                }
+                total_wrote_size += file_size;
+                count += 1;
+            }
+            let end = get_unix_timestamp();
+            end - start
+        },
+        HowToFill::WriteAll => {
+            let mut total_wrote_size = 0u64;
+            let start = get_unix_timestamp();
+            while total_wrote_size < total_garbage_size {
+                let mut file_path = file_path.clone();
+                file_path.push_str(count.to_string().as_str());
+                let mut file = File::create(file_path).unwrap();
+                file.write_all(&garbage_data).unwrap();
+                total_wrote_size += garbage_file_size;
+                count += 1;
+            }
+            let end = get_unix_timestamp();
+            end - start
+        },
+    };
+    println!("Заполнение длилось {} сек.", work_time);
+    pause();
 }
 
 fn get_unix_timestamp() -> u64 {
     std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs()
-}
-
-fn get_unix_milli_timestamp() -> u128 {
-    std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_millis()
 }
 
 //NOTE: Work only in Windows
@@ -106,4 +148,30 @@ fn get_input(prompt: &str) -> String {
         Err(_no_updates_is_fine) => {},
     }
     input.trim().to_string()
+}
+
+fn get_bytes_size(prompt: &str) -> u64 {
+    let chunk_size = get_input(prompt);
+    let mut number = String::new();
+    let mut kind = String::new();
+    for c in chunk_size.chars() {
+        if c.is_numeric() {
+            number.push(c);
+        } else {
+            kind.push(c)
+        }
+    }
+    let kind = kind.trim();
+    let number = number.trim().parse::<u64>().unwrap();
+    let coefficient: u64 = match kind.to_uppercase().as_str() {
+        "B" | "Б" => 2u64.pow(0),
+        "KIB" | "КИБ" => 2u64.pow(10),
+        "MIB" | "МИБ" => 2u64.pow(20),
+        "GIB" | "ГИБ" => 2u64.pow(30),
+        "KB" | "КБ" => 10u64.pow(3),
+        "MB" | "МБ" => 10u64.pow(6),
+        "GB" | "ГБ" => 10u64.pow(9),
+        _ => panic!("unknown")
+    };
+    number * coefficient
 }
